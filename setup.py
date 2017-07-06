0fff01ed702799a2f3e4ecbf2b85a01f2dbc49c7
Zeng-hans <1029181363@qq.com>
2017-07-06 17:06:42 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='HansZeng',
+    version='0.1dev',
+    packages=['myproject','myproject/tests'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
